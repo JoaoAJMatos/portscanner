e63e289a531449fcda321078bc8a650e149abe66
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2021-08-30 00:22:33 +0100

    Resolve Domain Name
    Introduced a function that resolves the IP for a domain name

--- portscanner.py
+++ portscanner.py
@@ -1,6 +1,13 @@
 import socket
 from IPy import IP
 
+def resolveDomain(target):
+    try: # Checks if the target argument is a valid IP address and returns it if 1
+        IP(target)
+        return(target)
+    except: # If the target isn't a valid IP address the function will try to resolve the IP
+        return socket.gethostbyname(target)
+
 def scanPort(target, port):
     try:
         sock = socket.socket()
@@ -10,7 +17,8 @@ def scanPort(target, port):
     except:
         print(f"[-] Port {str(port)} is closed")
 
-target = input('[+] Enter target hostname or IP: ')
+target   = input('[+] Enter target hostname or IP: ')
+targetIP = resolveDomain(target)
 
 for port in range(75, 85):
-    scanPort(target, port)
+    scanPort(targetIP, port)
