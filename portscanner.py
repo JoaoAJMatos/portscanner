a9d915702e9709a533bc3af0c7ac355ff6ec824b
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2021-08-30 12:11:03 +0100

    Converted Port Scanner to a Class
    The portscanner features can now be used in other programs py creating an instance of the PortScan class.

--- portscanner.py
+++ portscanner.py
@@ -1,46 +1,43 @@
+#------ PortScanner for vulnerability checking ------#
 import socket
 from IPy import IP
 
-def scan(target):
-    targetIP = resolveDomain(target)
-    print(f"\n[-] Scanning Target: {str(target)}")
+class PortScan():
+    banners   = []
+    openPorts = []
 
-    for port in range(1, 100):
-        scanPort(targetIP, port)
+    def __init__(self, target, ports):
+        self.target = target
+        self.ports = ports
 
-def resolveDomain(target):
-    try: # Checks if the target argument is a valid IP address and returns it if 1
-        IP(target)
-        return(target)
-    except: # If the target isn't a valid IP address the function will try to resolve the IP
-        return socket.gethostbyname(target)
 
-def getBanner(socket):
-    return socket.recv(1024)
+    def scan(self):
+        for port in range(1, 100):
+            self.scanPort(port)
 
-def scanPort(target, port):
-    try:
-        sock = socket.socket()
-        sock.settimeout(0.1)
-        sock.connect((target, port))
 
-        try: # Tries to get the service that's running on the port by attempting to get information about the banner of the service
-             # Warning: You wont be able to recieve the banner on most of the websites. 
-            banner = getBanner(sock)
-            print(f"[+] Open port {str(port)} running :" + str(banner.decode().strip('\n')))
+    def resolveDomain(self):
+        try: # Checks if the target argument is a valid IP address and returns it if 1
+            IP(self.target)
+            return(self.target)
+        except: # If the target isn't a valid IP address the function will try to resolve the IP
+            return socket.gethostbyname(self.target)
+
+
+    def scanPort(self, port):
+        try:
+            targetIP = self.resolveDomain()
+            sock = socket.socket()
+            sock.settimeout(0.1)
+            sock.connect((targetIP, port))
+            self.openPorts.append(port)
+
+            try: # Tries to get the service that's running on the port by attempting to get information about the banner of the service
+                # Warning: You wont be able to recieve the banner on most of the websites. 
+                self.banners.append(sock.recv(1024).decode().strip("\n").strip("\r"))
+            except:
+                self.banners.append(" ")
+
+            sock.close()
         except:
-            print(f"[+] Open port {str(port)}")
-    except:
-        #print(f"[-] Port {str(port)} is closed")
-        pass
-
-
-if __name__ == "__main__":
-
-    targets  = input("[+] Enter target/s hostname or IP (split targets with ','): ")
-
-    if "," in targets:
-        for target in targets.split(','):
-            scan(target.strip(' '))
-    else:
-        scan(targets)
+            pass
